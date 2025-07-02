593e1f9a8248cecacde2d2214adc0f92ec59ae10
Wessley Silva <wessley.silva@checkstore.com.br>
2025-07-01 21:22:11 -0300

    handle error and copy to clipboard btn

--- index.js
+++ index.js
@@ -36,6 +36,38 @@ input.addEventListener('keydown', function (ev) {
 })
 
 function calculate() {
+  resultInput.value = 'ERROR'
+  resultInput.classList.add('error')
   const result = eval(input.value)
   resultInput.value = result
+  resultInput.classList.remove('error')
 }
+
+document.getElementById('themeSwitcher').addEventListener('click', function () {
+  if(main.dataset.theme == 'dark') {
+    root.style.setProperty('--bg-color', '#f1f5f9')
+    root.style.setProperty('--font-color', '#212529')
+    root.style.setProperty('--border-color', '#aaa')
+    root.style.setProperty('--primary-color', '#26834a')
+    main.dataset.theme = 'light'
+  } else {
+    root.style.setProperty('--bg-color', '#212529')
+    root.style.setProperty('--font-color', '#f1f5f9')
+    root.style.setProperty('--border-color', '#666')
+    root.style.setProperty('--primary-color', '#4dff91')
+    main.dataset.theme = 'dark' 
+  }
+})
+
+document.getElementById('copyToClipboard').addEventListener('click', function(ev) {
+  const btn = ev.currentTarget
+  if(btn.innerText == 'Copy') {
+    btn.innerText = 'Copied'
+    btn.classList.add('success')
+    navigator.clipboard.writeText(resultInput.value)
+    setTimeout(() => {
+      btn.innerText = 'Copy'
+      btn.classList.remove('success')
+    }, 1000);
+  } 
+})
